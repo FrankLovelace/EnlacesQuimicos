b3ac2ce94f75cb4b92f6e1ae14957edb662cf28c
FrankLovelace <alexandrogallegos12@gmail.com>
2026-02-01 12:00:28 -0600

    forma de los datos en memoria

---
+++ C/db_types.h
@@ -0,0 +1,12 @@
+#ifndef DB_TYPES_H
+#define DB_TYPES_H
+
+#define MAX_SIMBOLO 5
+#define MAX_NOMBRE 50
+
+typedef struct {
+    char simbolo[MAX_SIMBOLO];
+    char nombre[MAX_NOMBRE];
+} ElementoInfo;
+
+#endif
